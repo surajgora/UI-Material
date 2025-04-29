b87d4cc6d4578e970933db4f331e0c8023d23ffc
Yogesh Anbhavane <112810507+Yogesh7977@users.noreply.github.com>
2025-04-29 22:16:21 +0530

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { CommonLayoutComponent } from './Ui-Material/common-layout/common-layout.component';
+import { ScrollAnimationComponent } from './Ui-Material/animation/scroll-animation/scroll-animation.component';
 
 export const routes: Routes = [
     {
         path:'',
         component:CommonLayoutComponent,
         children:[
+            {
+                path:'',
+                component:ScrollAnimationComponent
+            }
             // {
             //     path:"",
             //     loadChildren: () => import('../app/main-website/home/home.routes').then(r => r.HomeRoutes)
